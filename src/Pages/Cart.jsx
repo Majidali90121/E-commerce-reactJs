f8768ab0048155fa925e2c5ffb0aba431f352d91
Abdul Majid Ali Irshad <majidirshad04@gmail.com>
2025-07-16 17:52:28 -1200

    modifying cart

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,10 +1,10 @@
 import Cart from './Components/Cart';
 import { Link } from 'react-router-dom';
 import './Cart.css'
-function CartPage({cartItems,removeFromCart}){
+function CartPage({cartItems,removeFormCart}){
     return(
         <div className="cart-page">
-            <Cart cartItems={cartItems} removeFromCart={removeFromCart} />
+            <Cart cartItems={cartItems} removeFormCart={removeFormCart} />
             {cartItems.length > 0 && (
                 <div className="checkout-button-container">
                  <Link to='/checkout' className='checkout-button'>
